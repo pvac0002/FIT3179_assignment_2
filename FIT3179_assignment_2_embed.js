17895dca3a1f67013f87d0a1997fd12ffac495be
Pratham <pvac0002@student.monash.edu>
2025-10-12 13:45:06 +0000

    made graph 1 (unfinished)

--- FIT3179_assignment_2_embed.js
+++ FIT3179_assignment_2_embed.js
@@ -1,3 +1,7 @@
 var vg_1 = "FIT3179_Assignment_2_map1.vg.json";
 vegaEmbed("#map1", vg_1).then(function(result) {
 }).catch(console.error);
+
+var vg_2 = "FIT3179_Assignment_2_graph1.vg.json";
+vegaEmbed("#graph1", vg_2).then(function(result) {
+}).catch(console.error);
