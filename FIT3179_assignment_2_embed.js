f290173fa7647130d335d11bc1950b8586268a15
Pratham <pvac0002@student.monash.edu>
2025-10-12 10:34:04 +0000

    rename and small adjustments

--- FIT3179_assignment_2_embed.js
+++ FIT3179_assignment_2_embed.js
@@ -1,3 +1,3 @@
-var vg_1 = "FIT3179_assignment_2_embed.vg.json";
-vegaEmbed("map1", vg_1).then(function(result) {
+var vg_1 = "FIT3179_assignment_2_map1.vg.json";
+vegaEmbed("#map1", vg_1).then(function(result) {
 }).catch(console.error);
